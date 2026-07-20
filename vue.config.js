57c9457e59d6ec3d9997d091ab0c5af0097b11a0
Danilo Silva <contact@danilocgsilva.me>
2026-07-20 10:36:45 -0300

    Updated vue configurations

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const port = parseInt(process.env.PORT) || 8199;
+
+
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    port,
+    allowedHosts: 'all',
+    webSocketServer: 'ws',
+    client: {
+      webSocketURL: {
+	hostname: '0.0.0.0',
+	pathname: '/ws',
+	port,
+	protocol: 'ws'
+      },
+    },
+  },
+});
+
